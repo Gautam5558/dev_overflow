485746d3c63f0696b6de78e6e64164017b2ccc14
Gautam Jain <gautamjain5558@gmai.com>
2024-01-16 20:07:32 +0530

    Webhooks created commit to deploy our app to get webhooks working in clerk

---
+++ app/api/webhooks/route.ts
@@ -0,0 +1,92 @@
+/* eslint-disable camelcase */
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
+  if (!WEBHOOK_SECRET) {
+    throw new Error(
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+    );
+  }
+
+  // Get the headers
+  const headerPayload = headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Error occured -- no svix headers", {
+      status: 400,
+    });
+  }
+
+  // Get the body
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  // Create a new Svix instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET);
+
+  let evt: WebhookEvent;
+
+  // Verify the payload with the headers
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error occured", {
+      status: 400,
+    });
+  }
+
+  // Get the ID and type
+  if (evt.type === "user.created") {
+    const user = await createUser({
+      clerkId: evt.data.id,
+      username: evt.data.username,
+      name: `${evt.data.first_name}${
+        evt.data.last_name ? ` ${evt.data.last_name}` : ""
+      }`,
+      email: evt.data.email_addresses[0],
+      picture: evt.data.image_url,
+    });
+    return NextResponse.json({ status: "OK", user });
+  }
+
+  if (evt.type === "user.updated") {
+    const updatedUser = await updateUser({
+      clerkId: evt.data.id,
+      updateData: {
+        username: evt.data.username,
+        name: `${evt.data.first_name}${
+          evt.data.last_name ? ` ${evt.data.last_name}` : ""
+        }`,
+        email: evt.data.email_addresses[0],
+        picture: evt.data.image_url,
+      },
+      path: "/profile/" + evt.data.id,
+    });
+    return NextResponse.json({ status: "OK", updatedUser });
+  }
+
+  if (evt.type === "user.deleted") {
+    const deletedUser = await deleteUser({
+      clerkId: evt.data.id,
+    });
+    return NextResponse.json({ status: "OK", deletedUser });
+  }
+
+  return new Response("", { status: 200 });
+}
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,7 @@
 
 import { connectDb } from "../connectdb";
 import User from "../models/user.model";
+import Question from "../models/question.model";
 
 export const getUser = async (params: any) => {
   try {
@@ -9,6 +10,52 @@ export const getUser = async (params: any) => {
     const { userId } = params;
     const user = await User.findById(userId);
     return user;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const createUser = async (params: any) => {
+  try {
+    connectDb();
+    const newUser = new User({ ...params });
+    await newUser.save();
+    return newUser;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const updateUser = async (params: any) => {
+  try {
+    connectDb();
+    const updatedUser = await User.findOneAndUpdate(
+      { clerkId: params.clerkId },
+      { ...params.updateData },
+      { new: true }
+    );
+    // revalidate path of settings page todo
+
+    return updatedUser;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const deleteUser = async (params: any) => {
+  try {
+    connectDb();
+    const user = await User.findOne({ clerkId: params.clerkId });
+
+    //  const questionIds=await Question.find({author:user._id}).distinct("_id")
+
+    await Question.deleteMany({ author: user._id });
+
+    // TODO Delete answers corresponding to the question asked by the user
+    // TODO Also delete all answers from ansers collection corresponding top the user questions
+
+    const deletedUser = await User.deleteOne({ _id: user._id });
+    return deletedUser;
   } catch (err) {
     console.log(err);
   }
