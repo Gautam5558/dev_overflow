ffc130a23c15201179dc59f21a9ef9d45571a14d
Gautam Jain <gautamjain5558@gmail.com>
2024-02-06 13:09:41 +0530

    Fixing bug during deployment

--- app/(root)/jobs/page.tsx
+++ app/(root)/jobs/page.tsx
@@ -2,13 +2,13 @@ import AllJobs from "@/components/shared/AllJobs";
 import { Metadata } from "next";
 import React from "react";
 
-const metaData: Metadata = {
+export const metaData: Metadata = {
   title: "Jobs | dev_overflow",
   description:
     "Welcome to our Software Jobs Listing Page – your gateway to exciting career opportunities in the dynamic world of technology and software development!Whether you're a seasoned professional seeking your next challenge or a budding enthusiast eager to embark on your coding journey, our platform connects you with a diverse array of job openings tailored to your skills and interests.",
 };
 
-const Jobs = () => {
+export default function Jobs() {
   return (
     <section className="w-full">
       <div className="w-full">
@@ -17,8 +17,4 @@ const Jobs = () => {
       </div>
     </section>
   );
-};
-
-export default Jobs;
-
-export { metaData };
+}
